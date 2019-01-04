c976c02c3f8e2df34036174eeb26ea49af24531e
mdemir96 <19mdemir96@gmail.com>
2019-01-04 04:37:26 +0300

    initial commit

---
+++ controllers/api/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\servis\controllers\api;
+
+
+/**
+ * Default controller for the `servis` module
+ */
+class DefaultController extends \kouosl\base\controllers\api\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
